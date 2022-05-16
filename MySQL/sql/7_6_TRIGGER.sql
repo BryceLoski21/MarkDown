96dff06849decc09d88be841331fe1b52f31f2d5
BryceLoski21 <752358701@qq.com>
2022-05-16 15:40:50 +0800

    修改了7_6_TRIGGER.sql

--- MySQL/sql/7_6_TRIGGER.sql
+++ MySQL/sql/7_6_TRIGGER.sql
@@ -1,6 +1,7 @@
 --  修短随化, 终期于尽
 
 
+use choose;
 alter table choose drop foreign key choose_student_fk;
 alter table choose add foreign key student_course_fk
 foreign key(学号) references student(学号) on delete cascade;
@@ -15,3 +16,41 @@ for each row
 begin
 delete form choose where 学号=old.学号;
 end;$$
+
+-- 创建两张表: ORGANIZATION(父表)和MEMBER(子表)
+    -- 创建数据库book_management
+create database book_management;
+use book_management;
+
+create table ORGANIZATION(
+    O_NO int not null auto_increment,
+    O_NAME varchar(32) default '',
+    primary key(O_NO)
+)engine=InnoDB;
+
+create table MEMBER(
+    M_NO int not null auto_increment,
+    M_NAME varchar(32) default '',
+    O_NO int,
+    primary key(M_NO),
+    constraint ORGANIZATION_MEMBER_FK foreign key(O_NO) references ORGANIZATION(O_NO)
+)engine=InnoDB;
+
+    -- 向两张表插入记录
+insert into ORGANIZATION(O_NO, O_NAME) values
+(null, 'AA'),
+(null, 'BB');
+insert into MEMBER(M_NO, M_NAME, O_NO) values
+(null, '张', 1),
+(null, '王', 1),
+(null, '李', 1),
+(null, '赵', 2),
+(null, '孙', 2);
+
+    -- 创建触发器DELETE_TRIGGER: 当删除ORGANIZATION中某条记录前,先删除MEMBER中相关信息
+create trigger delete_trigger before delete on ORGANIZATION for each row
+begin
+delete from MEMBER where O_NO=OLD.O_NO;
+end;$$
+
+delimier ;
