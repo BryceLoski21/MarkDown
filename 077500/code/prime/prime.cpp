8ed835867890765c0a23a165d623c8f0decd9f6d
BryceLoski <752358701@qq.com>
2023-11-24 16:03:26 +0800

    修改了2017的848算法题代码： 每行输出10个质数后换行

--- 077500/code/prime/prime.cpp
+++ 077500/code/prime/prime.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int prime(int n) {
-	if (n <= 1)
-		return 0; // 必须输入大于1的整数
+	if (n <= 1) // 必须输入大于1的整数
+		return 0; 
 	for (int i = 2; i < n; i++) {
 		if (n % i == 0) 
 			return 0; // 不是质数
@@ -11,8 +11,15 @@ int prime(int n) {
 }
 
 int main() {
+	int count = 0; // 标志位，每行输出10个
 	for (int i = 1000; i > 1; i--) {
-		if (prime(i))
-			printf("%d\n", i);
+		if (prime(i)) {
+			printf("%d ", i);
+			count += 1;
+		}
+		if (count == 10) {
+			printf("\n");
+			count = 0;
+		}
 	}
 }
